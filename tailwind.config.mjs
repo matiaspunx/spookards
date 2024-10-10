d5d2ae6d283a7bf63a1e0986ea9fd992e5d74198
matias punx <matias.punx@gmail.com>
2024-10-10 14:05:23 -0300

    inicio del proyecto

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        marker: ['"Marker"', ...defaultTheme.fontFamily.sans],
+        emojis: ['"Emojis"', ...defaultTheme.fontFamily.sans],
+        mona: ['"Mona"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
