b1fb85fc8b40696d736b3d184a9d0e2ae7b62a95
matias punx <matias.punx@gmail.com>
2024-10-22 01:42:20 -0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, passthroughImageService } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -10,10 +8,28 @@ export default defineConfig({
   experimental: {
     contentLayer: true,
   },
+  build: {
+    inlineStylesheets: "always",
+  },
+  compressHTML: true,
+  prefetch: true,
   devToolbar: {
     enabled: false,
   },
   output: "server",
   adapter: vercel(),
   integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+    build: {
+      cssMinify: "lightningcss",
+      minify: true,
+    },
+  },
+  image: {
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+    service: passthroughImageService(),
+  },
 });
